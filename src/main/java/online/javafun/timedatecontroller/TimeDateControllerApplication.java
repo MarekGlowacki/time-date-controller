dbac07a3e89ca5f8648558dca2ad980007bc933d
User <user@its.me>
2023-01-18 07:01:18 +0100

    Initial commit

---
+++ src/main/java/online/javafun/timedatecontroller/TimeDateControllerApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.timedatecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDateControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeDateControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/javafun/timedatecontroller/TimeDateControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.timedatecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDateControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
